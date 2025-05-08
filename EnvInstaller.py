5fef8ab1c92e44db0ccb5d560767c81d03a38630
Krack <131531725+Krack777@users.noreply.github.com>
2025-05-08 23:30:53 +0300

    Update EnvInstaller.py

--- EnvInstaller.py
+++ EnvInstaller.py
@@ -6,7 +6,12 @@ def add_to_env(key, value, env_path=".env"):
 ClientID = str(input("Enter Client ID: "))
 ClientSecret = str(input("Enter Client Secret: "))
 TrackFlag = str(input("Enter Track-Flag Name: "))
-add_to_env("CLIENT_ID", ClientID)
-add_to_env("CLIENT_SECRET", ClientSecret)
-add_to_env("SPOTIPY_REDIRECT_URL", 'http://127.0.0.1:8888/callback')
-add_to_env("TrackFlag", TrackFlag)
+try:
+    add_to_env("CLIENT_ID", ClientID)
+    add_to_env("CLIENT_SECRET", ClientSecret)
+    add_to_env("SPOTIPY_REDIRECT_URL", 'http://127.0.0.1:8888/callback')
+    add_to_env("TrackFlag", TrackFlag)
+    print("Tokens and track-flag added successfully")
+except Exception as e:
+    print("somthing went wrong! Try to strart anything from begining")
+    print(f"Error: {e}")
